1a1b1c79755e6760bccc0506c2c9a6a0b20a73e3
Iris Ho <learncodecool@gmail.com>
2019-11-19 13:56:13 +0800

    update init

---
+++ index.js
@@ -0,0 +1,22 @@
+var linebot = require('calendarhelper');
+var express = require('express');
+
+var bot = linebot({
+  channelId: 1653541800,
+  channelSecret: '79745bae1556ad61175ff8ae2fd75956',
+//   channelAccessToken: channel Access Token
+});
+
+bot.on('message', function(event) {
+    console.log(event); //把收到訊息的 event 印出來看看
+  });
+  
+  const app = express();
+  const linebotParser = bot.parser();
+  app.post('/', linebotParser);
+  
+  //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+  var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
